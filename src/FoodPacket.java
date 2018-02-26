d27b3ee1eb9f6690c07365e0851cd1f4ec79b3bc
Babaganosch <lassemaja.92@gmail.com>
2018-02-27 00:07:41 +0100

    FoodPacket able to contain Food object now

--- src/FoodPacket.java
+++ src/FoodPacket.java
@@ -5,10 +5,20 @@ public class FoodPacket extends Packet{
 	private static final long serialVersionUID = 5424711620222586250L;
 
 	private HashMap<Integer, Food> foodList;
+	private Food food;
 	
 	public FoodPacket(int id, HashMap<Integer, Food> foodList) {
 		super(id);
 		this.foodList = foodList;
+	}
+	
+	private FoodPacket(int id, Food food) {
+		super(id);
+		this.food = food;
+	}
+	
+	public Food getFood() {
+		return food;
 	}
 	
 	public HashMap<Integer, Food> getFoodList() {
