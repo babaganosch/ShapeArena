ac25f5a5b3862154db5adc5cd63cd43df3d409cc
Babaganosch <lassemaja.92@gmail.com>
2018-03-04 00:43:32 +0100

    JavaDoc for Food.java

--- src/Food.java
+++ src/Food.java
@@ -1,5 +1,11 @@
 import java.io.Serializable;
 
+/**
+ * This is the Food class.
+ * Each object of this class represents a Food Object in the world.
+ * @author Hasse Aro
+ * @version 2018-03-xx
+ */
 public class Food implements Serializable {
 	
 	private static final long serialVersionUID = -8986608731006981204L;
@@ -7,28 +13,54 @@ public class Food implements Serializable {
 	private int x;
 	private int y;
 	
+	/**
+	 * Creates a Food object.
+	 * @param id The index of the Food object.
+	 * @param x The X coordinate of the Food object.
+	 * @param y The Y coordinate of the Food object.
+	 */
 	public Food(int id, int x, int y) {
 		this.id = id;
 		this.x = x;
 		this.y = y;
 	}
 	
+	/**
+	 * Getter for the index of the Food object.
+	 * @return Returns the index of the Food object.
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * Getter for the X coordinate of the Food object.
+	 * @return Returns the X coordinate of the Food object.
+	 */
 	public int getX() {
 		return this.x;
 	}
 	
+	/**
+	 * Getter for the Y coordinate of the Food object
+	 * @return Returns the Y coordinate of the Food object.
+	 */
 	public int getY() {
 		return this.y;
 	}
 	
+	/**
+	 * Setter of the X coordinate of the Food object.
+	 * @param x Sets the X coordinate of the Food object.
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 	
+	/**
+	 * Setter of the Y coordinate of the Food object.
+	 * @param y Sets the Y coordinate of the Food object.
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
--- src/Packet.java
+++ src/Packet.java
@@ -1,18 +1,36 @@
 import java.io.Serializable;
 
+/**
+ * This is the Packet class.
+ * This is a abstract class being the foundation for any type of packet.
+ * @author Hasse Aro
+ * @version 2018-03-xx
+ */
 public abstract class Packet implements Serializable {
 	
 	private static final long serialVersionUID = -8229001052268205876L;
 	private int id;
 	
+	/**
+	 * Creates a Packet object
+	 * @param id The id is mostly used as a playerID or identifier for the packet.
+	 */
 	public Packet(int id) {
 		this.id = id;
 	}
 	
+	/**
+	 * Getter for the id.
+	 * @return Returns the id from the packet.
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * Setter for the id.
+	 * @param id Sets the id.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
