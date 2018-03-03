d2f771d60529bb64c73b1f87bc02251c26fb92e1
Babaganosch <lassemaja.92@gmail.com>
2018-03-04 00:29:53 +0100

    JavaDoc for PlayerInitPacket and FoodPacket

--- src/PlayerInitializationPacket.java
+++ src/PlayerInitializationPacket.java
@@ -1,8 +1,18 @@
-
+/**
+ * This is the PlayerInitializationPacket class.
+ * This object is used as a packet sent over a Object Stream containing
+ * a playerID about a player for initialization purpose.
+ * @author Hasse Aro
+ * @version 2018-03-xx
+ */
 public class PlayerInitializationPacket extends Packet {
 
 	private static final long serialVersionUID = 4688858102876818865L;
 
+	/**
+	 * Creates a PlayerInitializationPacket
+	 * @param id The PlayerID
+	 */
 	public PlayerInitializationPacket(int id) {
 		super(id);
 	}
